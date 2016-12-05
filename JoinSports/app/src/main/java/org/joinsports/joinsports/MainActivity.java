2980da0a26387ad1a1bb56485858f1fb3ace7289
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2016-12-05 16:54:40 +0100

    Alex Implemented PHP database connector (DBDriver.java)

--- JoinSports/app/src/main/java/org/joinsports/joinsports/MainActivity.java
+++ JoinSports/app/src/main/java/org/joinsports/joinsports/MainActivity.java
@@ -1,7 +1,11 @@
 package org.joinsports.joinsports;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import org.joinsports.joinsports.DBDriver;
+import org.joinsports.joinsports.NormalUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //JUST DO IT
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    public void onClick_login(View v)
+    {
+        NormalUser user = new NormalUser();
+        user.setEmailAddress("@gmail");
+        user.setFirstName("Günther");
+        user.setLastName("Stängel");
+        user.setPassword("123456");
+        user.setUsername("günnie");
+
+        DBDriver dbd = DBDriver.getInstance();
+        dbd.createUser(user);
     }
 }

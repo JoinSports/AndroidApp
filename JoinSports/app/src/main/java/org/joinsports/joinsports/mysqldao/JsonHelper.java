9f9e148f9678cccc95d4892c860e55c45c188a39
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2017-04-25 00:39:10 +0200

    implemented JsonHelper class to simplify access of success and error values of the response json object

---
+++ JoinSports/app/src/main/java/org/joinsports/joinsports/mysqldao/JsonHelper.java
@@ -0,0 +1,45 @@
+package org.joinsports.joinsports.mysqldao;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Alex on 24.04.2017.
+ */
+
+public class JsonHelper {
+
+    public static boolean isSuccess(JSONObject json) {
+        try {
+            if (json.getString("status").equals("success")) return true;
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    };
+
+    public static int getErrorId(JSONObject json) {
+        try {
+            return json.getInt("errorId");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getErrorUserMsg(JSONObject json) {
+        try {
+            return json.getString("errorUserMsg");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static  String getErrorLogMsg(JSONObject json) {
+        try {
+            return json.getString("errorLogMsg");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}

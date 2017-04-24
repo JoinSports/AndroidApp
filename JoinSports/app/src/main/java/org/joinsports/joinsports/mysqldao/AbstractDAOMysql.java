5b7358bef6f03dbeb5c5a2c2573f80a140ebc0f4
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2017-04-25 00:37:44 +0200

    enhanced abstract class of all MySQL DAOs

--- JoinSports/app/src/main/java/org/joinsports/joinsports/mysqldao/AbstractDAOMysql.java
+++ JoinSports/app/src/main/java/org/joinsports/joinsports/mysqldao/AbstractDAOMysql.java
@@ -1,5 +1,7 @@
 package org.joinsports.joinsports.mysqldao;
 
+import org.json.JSONObject;
+
 /**
  * Created by Alex on 22.04.2017.
  */
@@ -7,9 +9,39 @@ package org.joinsports.joinsports.mysqldao;
 public abstract class AbstractDAOMysql {
 
     protected DBConnector dbConnector;
+    protected int errorId = 0;
+    protected String errorUserMsg = "";
+    protected String errorLogMsg = "";
+
+    protected boolean checkQuerySuccess(JSONObject responseJson) {
+        if (JsonHelper.isSuccess(responseJson)) {
+            //no error
+            errorId = 0;
+            errorUserMsg = "";
+            errorLogMsg = "";
+            return true;
+        } else {
+            errorId = JsonHelper.getErrorId(responseJson);
+            errorUserMsg = JsonHelper.getErrorUserMsg(responseJson);
+            errorLogMsg = JsonHelper.getErrorLogMsg(responseJson);
+            return false;
+        }
+    }
 
     public AbstractDAOMysql(DBConnector dbConnector) {
         this.dbConnector = dbConnector;
+    }
+
+    public int getErrorId() {
+        return errorId;
+    }
+
+    public String getErrorUserMsg() {
+        return errorUserMsg;
+    }
+
+    public String getErrorLogMsg() {
+        return errorLogMsg;
     }
 
 }

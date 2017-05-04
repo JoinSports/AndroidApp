870d49559387e31a68dde894b6d4727b9eac088a
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2017-05-04 16:22:46 +0200

    fixed existing code to use DAO interface instead

--- JoinSports/app/src/main/java/org/joinsports/joinsports/CreateTeam.java
+++ JoinSports/app/src/main/java/org/joinsports/joinsports/CreateTeam.java
@@ -8,8 +8,10 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.joinsports.joinsports.dao.TeamDAO;
 import org.joinsports.joinsports.entity.Team;
-import org.joinsports.joinsports.entity.TeamLeader;
+import org.joinsports.joinsports.mysqldao.DBConnector;
+import org.joinsports.joinsports.mysqldao.TeamDAOMysql;
 
 public class CreateTeam extends AppCompatActivity {
 
@@ -24,28 +26,22 @@ public class CreateTeam extends AppCompatActivity {
         System.out.println("##################");
         //get fields
         String teamname = ((EditText)findViewById(R.id.create_team_tf_teamname)).getText().toString();
-
-        // send data to DB & create user entry
-        TeamLeader teamLeader = new TeamLeader();
-        teamLeader.setUsername(Global.authusername);
-        teamLeader.setPassword(Global.authpasswort);
-        Team team = new Team(teamLeader);
+        Team team = new Team();
         team.setTeamName(teamname);
+        DBConnector dbc = new DBConnector(
+                Global.authusername, Global.authusername, Global.dbServerUrl);
+        TeamDAO teamDAO = new TeamDAOMysql(dbc);
+        boolean result = teamDAO.create(team, Global.userid);
 
-        DBDriver dbd = DBDriver.getInstance();
-        try {
-            dbd.createTeam(team);
-        }
-        catch (DatabaseException e) {
+        if (result) {
             TextView feedback = (TextView) findViewById(R.id.create_team_tv_feedback);
-            feedback.setText(e.getMessage());
+            feedback.setText("Team wurde erfolgreich erstellt.");
+            feedback.setTextColor(Color.BLACK);
+        } else {
+            TextView feedback = (TextView) findViewById(R.id.create_team_tv_feedback);
+            feedback.setText(teamDAO.getLastErrorUserMsg());
             feedback.setTextColor(Color.RED);
-            return; //abort function
         }
-
-        TextView feedback = (TextView) findViewById(R.id.create_team_tv_feedback);
-        feedback.setText("Team wurde erfolgreich erstellt.");
-        feedback.setTextColor(Color.BLACK);
 
         // change view: CreateUser-Activity --> StartScreen-Activity
         Intent k = new Intent(CreateTeam.this, StartScreen.class);

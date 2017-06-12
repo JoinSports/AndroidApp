0be4267afb16ec1019eff58fcae8329b7b535bbc
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2017-06-12 20:05:41 +0200

    added global data base connector variable

--- JoinSports/app/src/main/java/org/joinsports/joinsports/Global.java
+++ JoinSports/app/src/main/java/org/joinsports/joinsports/Global.java
@@ -1,5 +1,7 @@
 package org.joinsports.joinsports;
 
+import org.joinsports.joinsports.mysqldao.DBConnector;
+
 /**
  * Created by Alex on 13.12.2016.
  */
@@ -9,9 +11,12 @@ public class Global {
     //database connection information
     public static String dbServerUrl = "http://joinsports.hosting5858.af939.netcup.net";
 
+    //data base connector
+    public static DBConnector dbc = null;
+
     //user information
     public static String authusername = "";
     public static String authpasswort = "";
-    public static int userid = 0;
+    public static int userId = 0;
 
 }

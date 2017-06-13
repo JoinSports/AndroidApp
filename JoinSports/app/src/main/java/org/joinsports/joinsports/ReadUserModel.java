342e8970cc6f437ebd2275c5c9da10a6c926287c
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2017-06-13 22:28:17 +0200

    created and introduced model to fragment controller

---
+++ JoinSports/app/src/main/java/org/joinsports/joinsports/ReadUserModel.java
@@ -0,0 +1,15 @@
+package org.joinsports.joinsports;
+
+import org.joinsports.joinsports.entity.NormalUser;
+
+/**
+ * Created by Alex on 13.06.2017.
+ */
+
+public class ReadUserModel {
+
+    public NormalUser getUser() {
+        return Global.user;
+    };
+
+}

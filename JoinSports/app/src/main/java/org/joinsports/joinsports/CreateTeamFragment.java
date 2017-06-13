ac380f79efb9238c98c73e1621a3f667170b0f37
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2017-06-13 12:44:17 +0200

    created create team fragment

---
+++ JoinSports/app/src/main/java/org/joinsports/joinsports/CreateTeamFragment.java
@@ -0,0 +1,22 @@
+package org.joinsports.joinsports;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.joinsports.joinsports.utils.CustomFragment;
+
+
+public class CreateTeamFragment extends CustomFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_create_team, container, false);
+        return view;
+    }
+
+}
